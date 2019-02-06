aa09165dcc73cddd2adc74b4fd089ae852e0efcf
Julian Palladino <julianpalladino8@gmail.com>
2019-02-05 21:52:53 -0300

    Modifica set paths para cambiar la ruta absoluta de manera comoda

--- practicas/p9_escaneo3d/comun/set_paths.m
+++ practicas/p9_escaneo3d/comun/set_paths.m
@@ -1,12 +1,19 @@
 %% 3DP2018 dataset
-%calib_results_filename = 'imagenes/laser-plane/gray/Calib_Results.mat'; %
-%por algun motivo no anda la ruta relativa, hay que setearla absoluta
-calib_resuls_filename = '~/facultad/vision/vc-2018-2c/practicas/p9_escaneo3d/imagenes/laser-plane/gray/Calib_Results.mat';
-laser_color_images_path = 'imagenes/laser-plane/color';
-turntable_images_path = '~/facultad/vision/vc-2018-2c/practicas/p9_escaneo3d/imagenes/turntable-small/gray';
+
+% Por algun motivo no anda la ruta relativa, hay que setearla absoluta
+
+%%%% SETEAR ESTA DE FORMA ABSOLUTA:
+path_practica = '~/facultad/vision/vc-2018-2c/practicas/p9_escaneo3d/';
+
+
+%%%% NO TOCAR ESTAS:
+turntable_images_path = strcat(path_practica,'imagenes/turntable-small/gray');
+calib_images_path = strcat(path_practica,'imagenes/laser-plane/gray');
+caltag_path = strcat(path_practica,'comun/caltag');
+calib_resuls_filename = strcat(path_practica,'imagenes/laser-plane/gray/Calib_Results.mat');
+
 scanned_object_images_path = 'imagenes/turntable-small/color';
-calib_images_path = '~/facultad/vision/vc-2018-2c/practicas/p9_escaneo3d/imagenes/laser-plane/gray';
-caltag_path = '~/facultad/vision/vc-2018-2c/practicas/p9_escaneo3d/comun/caltag';
+laser_color_images_path = 'imagenes/laser-plane/color';
 %% add more
 
 
