448199a28171696da88d7e0b0dbef30cc714543d
Julián Palladino <julianpalladino8@gmail.com>
2019-03-14 11:12:50 -0300

    Comienza transfer learning en el tp final

---
+++ tp/build_dataset_transfer_learning.py
@@ -0,0 +1,169 @@
+from glob import glob
+import numpy as np
+from numpy.linalg import inv
+import os
+import shutil
+import random
+import cv2
+import gc
+import matplotlib.pyplot as plt
+from itertools import zip_longest
+import uuid
+from PIL import Image
+
+WIDTH, HEIGHT = 320, 240
+RHO = 32
+PATCH_SIZE = 128
+
+NUM_SAMPLES_PER_IMAGE = 10
+NUM_SAMPLES_PER_ARCHIVE = 10000
+
+NUM_IMAGES_PER_ARCHIVE = NUM_SAMPLES_PER_ARCHIVE // NUM_SAMPLES_PER_IMAGE
+
+
+def train_test_split(X, test_size=0.2):
+    n_test = round(len(X) * test_size)
+    random.shuffle(X)
+    test_samples, train_samples = X[:n_test], X[n_test:]
+    return train_samples, test_samples
+
+
+def random_patch_positions():
+    """Calculate random position for patch P"""
+    px = random.randint(RHO, WIDTH - PATCH_SIZE - RHO)
+    py = random.randint(RHO, HEIGHT - PATCH_SIZE - RHO)
+    return px, py  
+
+def random_delta():
+    """Calculate random delta for patch corner distortion"""
+    return np.random.randint(-RHO, RHO, size=2)
+
+def extract_from_patch(img, patch):
+    """Extract patch from image"""
+    a, _, c, _ = patch
+    return img[a[1]:c[1], a[0]:c[0],0:3]
+
+def build_random_patch():
+    px, py = random_patch_positions()
+    return [(px, py),
+            (px + PATCH_SIZE, py),
+            (px + PATCH_SIZE, py + PATCH_SIZE),
+            (px, py + PATCH_SIZE)]
+
+def distort_patch(patch):
+    return [p + random_delta() for p in patch]
+
+
+def get_homography(img, patch, new_patch):
+    # Get homography matrix
+    H = cv2.getPerspectiveTransform(
+        np.float32(patch),
+        np.float32(new_patch))
+
+    return H
+
+def transform_image(img,H):
+    # Transform image with H
+    return cv2.warpPerspective(img, inv(H),
+        (img.shape[1], img.shape[0]))  
+
+def process_image(img, debug=False):
+    # Resize and convert to grayscale
+    resized_img = cv2.resize(img, (WIDTH, HEIGHT))
+
+    if len(resized_img.shape) == 3:
+        # si era rgb, convertimos a gris
+        gray_img = cv2.cvtColor(resized_img, cv2.COLOR_RGB2GRAY)
+    else:
+        # si era gris, convertimos a rgb
+        gray_img = resized_img
+        resized_img = cv2.cvtColor(resized_img, cv2.COLOR_GRAY2RGB)
+    
+
+    assert(len(gray_img.shape) == 2)
+    assert(len(resized_img.shape) == 3)
+ 
+    # Build patch, and perturb corners
+    patch = build_random_patch()
+    distorted_patch = distort_patch(patch)
+
+    # We obtain H from the gray images
+    H = get_homography(gray_img, patch, distorted_patch)
+
+    # We apply to the RGB image
+    warped_img = transform_image(resized_img, H)
+   
+    # Extract patches
+    a_img = extract_from_patch(resized_img, patch)
+    b_img = extract_from_patch(warped_img, patch)
+    
+      # Stack patch images together
+    x = np.dstack([a_img, b_img])
+    
+    
+
+    # Subtract patches to get delta
+    # int8 is good enough because RHO = 32, so, values are in [-32, 32] range
+    y = np.subtract(patch, distorted_patch).ravel().astype(np.int8)
+
+    if debug:
+        # On debug mode, return also original patch and warped img for plotting
+        return x, y, (patch, distorted_patch), (gray_img, warped_img)
+    
+    return x, y
+
+
+def grouper(iterable, n, fillvalue=None):
+    "Collect data into fixed-length chunks or blocks"
+    # grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx"
+    args = [iter(iterable)] * n
+    return zip_longest(*args, fillvalue=fillvalue)
+
+def generate_dataset_archives(dirname, X_train, X_val):
+    if os.path.exists(dirname):
+        print('{} already exists! Delete it if you want to regenerate archives.'.format(dirname))
+        return
+    
+    datasets = dict(train=X_train, val=X_val)
+
+    
+    for key, X_set in datasets.items():
+        random.shuffle(X_set)
+        
+        for img_group in grouper(X_set, NUM_IMAGES_PER_ARCHIVE):
+            xs, ys = [], []
+
+            cant = 0
+            for img_path in img_group: #['test2017/000000368552.jpg']:
+                if img_path:
+                    for i in range(NUM_SAMPLES_PER_IMAGE):
+                        img = plt.imread(img_path)
+                        if (cant % 100 == 0):
+                            print ("Processing image N° " + str(cant))
+                        cant = cant + 1
+                        try:
+                            x, y = process_image(img)
+                            xs.append(x)
+                            ys.append(y)
+                        except:
+                            print("Image broken? {}".format(img_path))
+
+            xs = np.array(xs, dtype=np.uint8)
+            ys = np.array(ys, dtype=np.int8)
+            
+            outfile = os.path.join(dirname, key, '{}.npz'.format(uuid.uuid4()))
+            os.makedirs(os.path.dirname(outfile), exist_ok=True)
+            np.savez_compressed(outfile, x=xs, y=ys)
+            print(outfile)
+
+
+DATA_DIR = os.path.join('')
+print(DATA_DIR)
+X = glob(os.path.join(DATA_DIR, 'test2017', '*.jpg'))
+
+
+
+X_train, X_val = train_test_split(X, test_size=0.2)
+print(len(X_train), len(X_val))
+
+generate_dataset_archives(os.path.join(DATA_DIR, 'datasetColor'), X_train, X_val)
---
+++ tp/train_transfer_learning.py
@@ -0,0 +1,249 @@
+import tensorflow as tf
+import tensorflow.keras as keras
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.layers import Dense, Flatten, Dropout, MaxPooling2D, Input, BatchNormalization,concatenate, Lambda
+from tensorflow.keras.layers import Convolution2D as Conv2D
+from tensorflow.keras.optimizers import SGD, Adam
+from tensorflow.keras.callbacks import ReduceLROnPlateau, ModelCheckpoint, LearningRateScheduler, TensorBoard
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import tensorflow.keras.backend as K
+from glob import glob
+import numpy as np
+import os
+import shutil
+import random
+import cv2
+import gc
+import matplotlib.pyplot as plt
+from itertools import zip_longest
+import uuid
+from multiprocessing import Pool, cpu_count
+from datetime import datetime
+from tensorflow.keras.optimizers import Adagrad
+import mobilenet_v2
+
+
+
+WIDTH, HEIGHT = 320, 240
+RHO = 32
+PATCH_SIZE = 128
+# FIXME
+DATA_DIR = os.path.join('')
+
+
+""" session """
+from tensorflow.keras.backend import set_session
+config = tf.compat.v1.ConfigProto()
+config.gpu_options.allow_growth = True  # dynamically grow the memory used on the GPU
+#config.log_device_placement = True  # to log device placement (on which device the operation ran)
+
+sess = tf.compat.v1.Session(config= config) 
+
+set_session(sess)  # set this TensorFlow session as the default session for Keras
+
+
+
+
+def preprocess_features(x):
+    # Rescale images
+    #x = x / 255.0
+    # Sample-wise centering
+    #x -= np.mean(x, axis=0, keepdims=True)
+    # Sample-wise std normalization
+    #x /= (np.std(x, axis=0, keepdims=True) + 1e-7)    
+    x = (x - 127.5) / 127.5
+    return x
+
+def preprocess_labels(y):
+    # Rescale
+    y = y / RHO
+    return y
+
+def build_data_generator(path, batch_size=64):
+    while True:
+        files = glob(os.path.join(path, '*.npz'))
+        random.shuffle(files)
+        
+        for npz in files:
+            archive = np.load(npz)
+            xs = archive['x']
+            ys = archive['y']
+
+            # Yield minibatches
+            for i in range(0, len(xs), batch_size):
+                end_i = min(i + batch_size, len(xs))
+                xs_batch = xs[i:end_i]
+                ys_batch = ys[i:end_i]
+                
+                # Preprocess features and labels
+                xs_batch = preprocess_features(xs_batch)
+                ys_batch = preprocess_labels(ys_batch)
+
+                yield [xs_batch[:,:,:,0:3],xs_batch[:,:,:,3:6]], ys_batch
+
+def euclidean_l2_loss(y_true, y_pred):
+    diff = K.reshape(y_true, (-1,4,2)) - K.reshape(y_pred, (-1,4,2))
+    return K.sqrt(K.sum(K.square(diff), axis=2))
+
+def mace(y_true, y_pred):
+    """Mean Average Corner Error metric"""
+    diff = K.reshape(y_true * RHO, (-1,4,2)) - K.reshape(y_pred * RHO, (-1,4,2))
+    return K.mean(K.sqrt(K.sum(K.square(diff), axis=2)))  
+
+def homography_regression_model():
+    input_shape = (128, 128, 2)
+    filters = 64
+    kernel_size = (3, 3)
+    conv_strides = (1, 1)
+    
+    input_img = Input(shape=input_shape)
+     
+    x = Conv2D(filters, kernel_size, strides=conv_strides, padding='same', name='conv1', activation='relu')(input_img)
+    x = BatchNormalization()(x)
+    x = Conv2D(filters, kernel_size, strides=conv_strides, padding='same', name='conv2', activation='relu')(x)
+    x = BatchNormalization()(x)
+    x = MaxPooling2D(pool_size=(2, 2), strides=(2, 2), name='pool1')(x)
+    
+    x = Conv2D(filters, kernel_size, strides=conv_strides, padding='same', name='conv3', activation='relu')(x)
+    x = BatchNormalization()(x)    
+    x = Conv2D(filters, kernel_size, strides=conv_strides, padding='same', name='conv4', activation='relu')(x)
+    x = BatchNormalization()(x)
+    x = MaxPooling2D(pool_size=(2, 2), strides=(2, 2), name='pool2')(x)
+   
+    x = Conv2D(filters*2, kernel_size, strides=conv_strides, padding='same', name='conv5', activation='relu')(x)
+    x = BatchNormalization()(x)    
+    x = Conv2D(filters*2, kernel_size, strides=conv_strides, padding='same', name='conv6', activation='relu')(x)
+    x = BatchNormalization()(x)
+    x = MaxPooling2D(pool_size=(2, 2), strides=(2, 2), name='pool3')(x)
+    
+    x = Conv2D(filters*2, kernel_size, strides=conv_strides, padding='same', name='conv7', activation='relu')(x)
+    x = BatchNormalization()(x)    
+    x = Conv2D(filters*2, kernel_size, strides=conv_strides, padding='same', name='conv8', activation='relu')(x)
+    x = BatchNormalization()(x)
+
+    x = Flatten()(x)
+    x = Dropout(0.5)(x)    
+    x = Dense(1024, name='fc1', activation='relu')(x)
+    x = Dropout(0.5)(x)
+    out = Dense(8, name='fc2', activation=None)(x)
+    
+    model = Model(inputs=input_img, outputs=[out])
+    
+    model.compile(optimizer=SGD(lr=0.005, momentum=0.9),
+                  loss=euclidean_l2_loss,
+                  metrics=['mse', mace])
+
+    return model
+
+
+def train(model, initial_epoch=None):
+    batch_size = 16
+
+    iterations_per_stage = 50000
+    stages = 4
+    total_iterations = iterations_per_stage * stages
+    
+    # FIXME
+    n_total = len(glob(os.path.join(DATA_DIR, 'test2017', '*.jpg')))
+    n_val = round(n_total * 0.2)
+    n_train = n_total - n_val
+    print(n_total)
+    print(n_val)
+
+
+    steps_per_epoch = n_train // batch_size
+    epochs = total_iterations // steps_per_epoch
+    epochs_per_stage = iterations_per_stage // steps_per_epoch
+
+    print("epochs:", epochs)
+    print("epochs per stage:", epochs_per_stage)
+
+    # Callbacks
+    def scheduler_fn(epoch, lr):
+        if epoch > 0 and epoch % epochs_per_stage == 0:
+            return lr * 0.1
+        else:
+            return lr
+    scheduler = LearningRateScheduler(scheduler_fn, verbose=1)
+    checkpoint = ModelCheckpoint('checkpoint.h5', monitor='val_loss', verbose=1, save_best_only=True, save_weights_only=False)
+    
+    tb_log_dir = os.path.join("logs", "{}".format(datetime.now().strftime("%Y%m%d_%H%M%S")))
+    os.makedirs(tb_log_dir, exist_ok=True)
+    tensorboard = TensorBoard(log_dir=tb_log_dir, write_images=True)
+
+    # Build data generators
+    train_dir = os.path.join(DATA_DIR, 'datasetColor', 'train')
+    val_dir = os.path.join(DATA_DIR, 'datasetColor', 'val')
+    train_generator = build_data_generator(train_dir, batch_size)
+    val_generator = build_data_generator(val_dir, batch_size)
+
+    # Fit!
+    model.fit_generator(train_generator,
+        initial_epoch=initial_epoch,
+        steps_per_epoch=steps_per_epoch,
+        epochs=epochs,
+        validation_data=val_generator,
+        validation_steps=(n_val // batch_size),
+        callbacks = [scheduler, checkpoint, tensorboard])
+
+
+def homography_regression_model_transfer_learning():
+    input_shape = (128, 128, 3)
+    filters = 64
+    kernel_size = (3, 3)
+    conv_strides = (1, 1)
+    
+
+    ### ---------- mobilenet 1
+    input_img1 = Input(shape=input_shape)
+    mobileNet1 = mobilenet_v2.MobileNetV2(input_shape=input_shape, \
+                    alpha=1.0, include_top=False, weights="imagenet",layerNameSuffix="_1",\
+                    backend = tf.keras.backend, layers = tf.keras.layers, models = tf.keras.models, utils = tf.keras.utils)
+    
+    out1 = Flatten()(mobileNet1.output)
+    inp1 = mobileNet1.input
+    modelMobileNet1 = Model(inp1, out1, name="mobileNet1")
+    for layer in modelMobileNet1.layers:
+        layer.trainable = False
+
+
+    ### ---------- mobilenet 2
+    input_img2 = Input(shape=input_shape)
+    mobileNet2 = mobilenet_v2.MobileNetV2(input_shape=input_shape, \
+                    alpha=1.0, include_top=False, weights="imagenet",layerNameSuffix="_2",\
+                    backend = tf.keras.backend, layers = tf.keras.layers, models = tf.keras.models, utils = tf.keras.utils)
+    out2 = Flatten()(mobileNet2.output)
+    inp2 = mobileNet2.input
+    modelMobileNet2 = Model(inp2, out2, name="mobileNet2")
+    for layer in modelMobileNet2.layers:
+        layer.trainable = False
+
+
+    ### merge
+    merged = concatenate([out1, out2])
+    
+
+    ### we add dense layers + dropouts
+    x = Dense(8, name='fc2_2', activation=None)(merged)
+    x = Dropout(0.5)(x)    
+    x = Dense(8, name='fc1_2', activation='relu')(x)
+    x = Dropout(0.5)(x)
+
+    model = Model(inputs=[modelMobileNet1.input,modelMobileNet2.input], outputs=[x])
+    
+    model.compile(optimizer=SGD(lr=0.005, momentum=0.9),
+                  loss=euclidean_l2_loss,
+                  metrics=['mse', mace])
+
+    return model
+
+if __name__ == "__main__":
+    import sys
+
+    model = homography_regression_model_transfer_learning()
+
+    print(model.summary())
+
+    print("------------- Training -------------")
+    train(model, initial_epoch=0)
+
