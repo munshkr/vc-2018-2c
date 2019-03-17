46779c75d1704ed6a0781d5f7843888b7b465fea
Julián Palladino <julianpalladino8@gmail.com>
2019-03-16 22:25:35 -0300

    Sigue andando poco, aunque mejor que antes

--- tp/train_transfer_learning.py
+++ tp/train_transfer_learning.py
@@ -95,9 +95,9 @@ def train(model, initial_epoch=None):
     total_iterations = iterations_per_stage * stages
 
     # FIXME
-    #_total = len(glob(os.path.join(DATA_DIR, 'test2017', '*.jpg')))
+    n_total = len(glob(os.path.join(DATA_DIR, 'test2017', '*.jpg')))
     #n_total = len(glob(os.path.join(DATA_DIR, 'dataset', '*', '*.jpg')))
-    n_total = 10000
+    #n_total = 10000
     n_val = round(n_total * 0.2)
     n_train = n_total - n_val
     print(n_total)
