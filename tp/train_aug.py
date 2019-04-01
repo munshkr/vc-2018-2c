56d380ca3a20c4a58a0598ce8753e8daa24cd8a9
Damián Silvani <munshkr@gmail.com>
2019-03-31 23:52:42 -0300

    tp: Probando imgaug

--- tp/train_aug.py
+++ tp/train_aug.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 from keras.models import Sequential, Model
 from keras.layers import Dense, Flatten, Dropout, MaxPooling2D, Input, BatchNormalization
 from keras.layers.convolutional import Conv2D
@@ -27,12 +28,7 @@ DATA_DIR = os.path.join('')
 
 
 def preprocess_features(x):
-    # Rescale images
-    #x = x / 255.0
-    # Sample-wise centering
-    #x -= np.mean(x, axis=0, keepdims=True)
-    # Sample-wise std normalization
-    #x /= (np.std(x, axis=0, keepdims=True) + 1e-7)    
+    # Recale and centering
     x = (x - 127.5) / 127.5
     return x
 
@@ -45,7 +41,7 @@ def build_data_generator(path, batch_size=64):
     while True:
         files = glob(os.path.join(path, '*.npz'))
         random.shuffle(files)
-        
+
         for npz in files:
             archive = np.load(npz)
             xs = archive['x']
@@ -56,7 +52,7 @@ def build_data_generator(path, batch_size=64):
                 end_i = min(i + batch_size, len(xs))
                 xs_batch = xs[i:end_i]
                 ys_batch = ys[i:end_i]
-                
+
                 # Preprocess features and labels
                 xs_batch = preprocess_features(xs_batch)
                 ys_batch = preprocess_labels(ys_batch)
@@ -70,47 +66,47 @@ def euclidean_l2_loss(y_true, y_pred):
 def mace(y_true, y_pred):
     """Mean Average Corner Error metric"""
     diff = K.reshape(y_true * RHO, (-1,4,2)) - K.reshape(y_pred * RHO, (-1,4,2))
-    return K.mean(K.sqrt(K.sum(K.square(diff), axis=2)))  
+    return K.mean(K.sqrt(K.sum(K.square(diff), axis=2)))
 
 def homography_regression_model():
     input_shape = (128, 128, 2)
     filters = 64
     kernel_size = (3, 3)
     conv_strides = (1, 1)
-    
+
     input_img = Input(shape=input_shape)
-     
+
     x = Conv2D(filters, kernel_size, strides=conv_strides, padding='same', name='conv1', activation='relu')(input_img)
     x = BatchNormalization()(x)
     x = Conv2D(filters, kernel_size, strides=conv_strides, padding='same', name='conv2', activation='relu')(x)
     x = BatchNormalization()(x)
     x = MaxPooling2D(pool_size=(2, 2), strides=(2, 2), name='pool1')(x)
-    
+
     x = Conv2D(filters, kernel_size, strides=conv_strides, padding='same', name='conv3', activation='relu')(x)
-    x = BatchNormalization()(x)    
+    x = BatchNormalization()(x)
     x = Conv2D(filters, kernel_size, strides=conv_strides, padding='same', name='conv4', activation='relu')(x)
     x = BatchNormalization()(x)
     x = MaxPooling2D(pool_size=(2, 2), strides=(2, 2), name='pool2')(x)
-   
+
     x = Conv2D(filters*2, kernel_size, strides=conv_strides, padding='same', name='conv5', activation='relu')(x)
-    x = BatchNormalization()(x)    
+    x = BatchNormalization()(x)
     x = Conv2D(filters*2, kernel_size, strides=conv_strides, padding='same', name='conv6', activation='relu')(x)
     x = BatchNormalization()(x)
     x = MaxPooling2D(pool_size=(2, 2), strides=(2, 2), name='pool3')(x)
-    
+
     x = Conv2D(filters*2, kernel_size, strides=conv_strides, padding='same', name='conv7', activation='relu')(x)
-    x = BatchNormalization()(x)    
+    x = BatchNormalization()(x)
     x = Conv2D(filters*2, kernel_size, strides=conv_strides, padding='same', name='conv8', activation='relu')(x)
     x = BatchNormalization()(x)
 
     x = Flatten()(x)
-    x = Dropout(0.5)(x)    
+    x = Dropout(0.5)(x)
     x = Dense(1024, name='fc1', activation='relu')(x)
     x = Dropout(0.5)(x)
     out = Dense(8, name='fc2', activation=None)(x)
-    
+
     model = Model(inputs=input_img, outputs=[out])
-    
+
     model.compile(optimizer=SGD(lr=0.005, momentum=0.9),
                   loss=euclidean_l2_loss,
                   metrics=['mse', mace])
@@ -124,7 +120,7 @@ def train(model, initial_epoch=None):
     iterations_per_stage = 50000
     stages = 4
     total_iterations = iterations_per_stage * stages
-    
+
     # FIXME
     n_total = len(glob(os.path.join(DATA_DIR, 'test2017', '*.jpg')))
     n_val = round(n_total * 0.2)
@@ -145,7 +141,7 @@ def train(model, initial_epoch=None):
             return lr
     scheduler = LearningRateScheduler(scheduler_fn, verbose=1)
     checkpoint = ModelCheckpoint('checkpoint.h5', monitor='val_loss', verbose=1, save_best_only=True, save_weights_only=False)
-    
+
     tb_log_dir = os.path.join("logs", "{}".format(datetime.now().strftime("%Y%m%d_%H%M%S")))
     os.makedirs(tb_log_dir, exist_ok=True)
     tensorboard = TensorBoard(log_dir=tb_log_dir, write_images=True)
